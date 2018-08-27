7f09f7f8b33dae81a42faddbd96e069a96aa1d11
ryota717 <ryota0717rocky@gmail.com>
2018-08-27 17:33:47 +0900

    test for flow.py

---
+++ test.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+
+from numba import jit
+from flow import *
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from skimage import io
+from sklearn.utils.linear_assignment_ import linear_assignment
+import glob
+import time
+import argparse
+import cv2
+
+video_path = input("input the video name : ")
+vid = cv2.VideoCapture(video_path)
+
+frame_h, frame_w = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+mot_tracker = Sort()
+
+mask = np.ones((frame_h,frame_w),dtype=np.uint8)
+old_gray = np.zeros_like(mask)
+dets = np.array([[831,156,1002,249,100]], dtype = np.uint16)
+num = 0
+
+while True:
+    ret, frame = vid.read()
+
+    if not ret:
+        break
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if num:
+        mask, track_bbs_ids = mot_tracker.update(dets, old_gray, frame_gray, mask)
+        print(track_bbs_ids)
+    old_gray = frame_gray
+
+    if num == 1:
+        for i in dets:
+            mask[i[1]:i[3], i[0]:i[2]] += 1
+
+    if num == 2:
+        dets = []
+
+    num += 1
